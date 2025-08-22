6fc3688e745b9f31e8c39d089cee4a426681b01f
.unitz01 <167648625+lucianavfxdude@users.noreply.github.com>
2025-08-22 14:33:44 -0400

    Create VynixuReUploadload.lua

---
+++ VynixuReUploadload.lua
@@ -0,0 +1,8 @@
+local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/lucianavfxdude/vynixureupload2025/refs/heads/main/Source.lua"))()
+module.GiveCrucifix({
+    Type = 1,
+    Uses = nil, -- nil : infinite uses
+    Resist = false,
+    EntitiesOnly = false, -- true : custom entities only
+    IgnoreList = {}
+})
